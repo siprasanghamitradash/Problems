03accf0a20942420a05d8fa8fb2921f095aa1115
Sipra <dash.sipra.sanghamitra@gmail.com>
2026-09-02 19:20:58 +0530

    Create OptimisedSolution.cpp

---
+++ C++/628. Maximum Product Of Three Numbers /OptimisedSolution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int n = nums.size();
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;
+        int max3 = INT_MIN;
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+        for(int i=0;i<n;i++){
+            if(nums[i]>max1){
+                max3=max2;
+                max2=max1;
+                max1=nums[i];
+            }else if(nums[i]>max2){
+                max3=max2;
+                max2=nums[i];
+            }else if(nums[i]>max3){
+                max3=nums[i];
+            }
+            if(nums[i]<min1){
+                min2 = min1;
+                min1=nums[i];
+            }else if(nums[i]<min2){
+                min2=nums[i];
+            }
+        }
+        int p1 = max1*max2*max3;
+        int p2= max1*min1*min2;
+        if(p2>p1){
+            return p2;
+        }else{
+            return p1;
+        }
+    }
+};
